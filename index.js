24b9620865888a5aab01d321704f91bba75f0408
Greg Driza <gdriza@uarts.edu>
2017-09-13 17:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function increaseRankBy(n)
 
   
 }
+
+function deepestChild()
+{
+  //Define a function `deepestChild()` that pulls out the most deeply nested child
+  //from `div#grand-node`. (Remember, you can iterate over elements and call
+  //`querySelector()` and `querySelectorAll()` on them. This is challenging to
+  //implement correctly, but not beyond your ability!)
+
+
+  
+}
