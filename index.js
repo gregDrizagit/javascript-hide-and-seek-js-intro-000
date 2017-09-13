03c941dca71a3dfcd7325ebf131d89d5203f47b8
Greg Driza <gdriza@uarts.edu>
2017-09-13 23:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function nestedTarget()
 
 function increaseRankBy(n)
 {
-  const getclass = document.querySelector('ul.ranked-list');
-  var lis1 = getclass.getElementsByTagName('li');
+  var rankedListArray = document.getElementsByClassName('ranked-list');
+  //var lis1 = getclass.getElementsByTagName('li');
   //var printThis = lis1.length;
 
   for(let i = 0; i < lis1.length; i++)
